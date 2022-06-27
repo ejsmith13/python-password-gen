7493da954d00c12b1b80c44d8d6caf161bcf9cb7
ejsmith13 <ejsmithwork@gmail.com>
2022-06-26 21:56:58 -0400

    created lists for characters

---
+++ main.py
@@ -0,0 +1,16 @@
+import string
+
+
+def main():
+
+    lower = list(string.ascii_lowercase)
+    upper = list(string.ascii_uppercase)
+    numeral = list(string.digits)
+    special = ["!", "?", "$", "&", "/", "-", "="]
+    print(lower)
+    print(upper)
+    print(numeral)
+    print(special)
+
+
+main()
